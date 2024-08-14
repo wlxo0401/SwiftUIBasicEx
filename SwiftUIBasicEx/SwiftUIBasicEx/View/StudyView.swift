40761345574ca2015e86ecca977309588979ba8b
KimJiTae <wlxo0401@gmail.com>
2024-08-14 18:44:11 +0900

    커밋

--- SwiftUIBasicEx/SwiftUIBasicEx/View/StudyView.swift
+++ SwiftUIBasicEx/SwiftUIBasicEx/View/StudyView.swift
@@ -19,10 +19,32 @@ struct StudyView: View {
             NavigationStack {
                 List {
                     NavigationLink("Red", destination: Text("Red"))
+                        .navigationBarBackButtonHidden()
                     NavigationLink("Green", destination: Text("Green"))
                     NavigationLink("Blue", destination: Text("Blue"))
                 }
+                .navigationDestination(for: Color.self, destination: { color in
+                    TestView()
+                })
+                .navigationTitle("Colors")
             }
+            .navigationBarBackButtonHidden()
         }
+    }
+}
+
+struct TestView: View {
+    var body: some View {
+        Text("안녕하세요.")
+    }
+}
+
+
+struct ColorDetail: View {
+
+    var color: Color
+    
+    var body: some View {
+            Text("\(self.color.description)")
     }
 }
